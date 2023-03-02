f405c2b03875ec44e88cb901e8f4ca4594243a73
valberjr <3cifhzht@duck.com>
2023-03-02 17:30:35 -0300

    database connection config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm/dist';
+import { typeOrmConfig } from './configs/typeorm.config';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig)],
   controllers: [],
   providers: [],
 })
