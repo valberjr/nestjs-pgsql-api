38e8eb072e82effa935ada469c7c2ef756fe6790
valberjr <3cifhzht@duck.com>
2023-03-07 14:23:19 -0300

    added id

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -44,7 +44,13 @@ export class UserRepository extends Repository<User> {
     query.skip((queryDto.page - 1) * queryDto.limit);
     query.take(+queryDto.limit);
     query.orderBy(queryDto.sort ? JSON.parse(queryDto.sort) : undefined);
-    query.select(['user.name', 'user.email', 'user.role', 'user.status']);
+    query.select([
+      'user.id',
+      'user.name',
+      'user.email',
+      'user.role',
+      'user.status',
+    ]);
 
     const [users, total] = await query.getManyAndCount();
 
